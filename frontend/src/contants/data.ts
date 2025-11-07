97c57c8304b7a03cd79aa0dfd1379fece39797cf
huywb <pthhuy1306@gmail.com>
2025-11-07 16:24:03 +0700

    List data menu homePage

--- frontend/src/contants/data.ts
+++ frontend/src/contants/data.ts
@@ -1,6 +1,6 @@
 
 
-const MENU_ITEMS = [
+export const MENU_ITEMS = [
     { name: 'Men', link: '/men' },
     { name: 'Women', link: '/women' },
     { name: 'Top Wear', link: '/topwear' },
@@ -8,4 +8,62 @@ const MENU_ITEMS = [
 ];
 
 
-export default MENU_ITEMS;
+export const CART_ITEMS = [
+    {
+        productId: 1,
+        name: 'T-shirt',
+        size: 'M',
+        color: 'Red',
+        quantity: 1,
+        price: 15,
+        image: 'https://picsum.photos/200?random=1'
+    },
+    {
+        productId: 2,
+        name: 'Jeans',
+        size: 'L',
+        color: 'Blue',
+        quantity: 1,
+        price: 5,
+        image: 'https://picsum.photos/200?random=2'
+    },
+    {
+        productId: 3,
+        name: 'Hoodie',
+        size: 'S',
+        color: 'Purple',
+        quantity: 2,
+        price: 35,
+        image: 'https://picsum.photos/200?random=3'
+    },
+    {
+        productId: 4,
+        name: 'Dress',
+        size: 'S',
+        color: 'Green',
+        quantity: 20,
+        price: 25,
+        image: 'https://picsum.photos/200?random=4'
+    }
+]
+
+export const SHOP_LINKS = [
+    { name: "Men's Top Wear", link: '/mens-topwear' },
+    { name: "Women's Top Wear", link: '/womens-topwear' },
+    { name: "Men's Bottom Wear", link: '/mens-bottomwear' },
+    { name: "Women's Bottom Wear", link: '/womens-bottomwear' }
+]
+
+export const SUPPORT_LINKS = [
+    { name: 'Help Center', link: '/help-center' },
+    { name: 'Returns', link: '/returns' },
+    { name: 'Shipping Info', link: '/shipping-info' },
+    { name: 'Contact Us', link: '/contact-us' }
+]
+
+export const COMPANY_LINKS = [
+    { name: 'About Us', link: '/about-us' },
+    { name: 'Careers', link: '/careers' },
+    { name: 'Press', link: '/press' },
+    { name: 'Blog', link: '/blog' }
+]
