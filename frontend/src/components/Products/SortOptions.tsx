cf3b9b62312fe16d7ef47727b9c8d9435b00c4e3
huywb <pthhuy1306@gmail.com>
2025-11-15 16:22:29 +0700

    SortOptions component

---
+++ frontend/src/components/Products/SortOptions.tsx
@@ -0,0 +1,28 @@
+import { useSearchParams } from "react-router-dom"
+
+const SortOptions = () => {
+    const [searchParams,setSearchParams] = useSearchParams()
+
+    const handleSortChange = (e:any)=>{
+        const sortBy = e.target.value
+        searchParams.set("sortBy",sortBy)
+        setSearchParams(searchParams)
+    }
+  return (
+    <div className="mb-4 flex items-center justify-end">
+        <select 
+        id="sort" 
+        className="border p-2 rounded-md focus:outline-none"
+        value={searchParams.get('sortBy') || ''}
+        onChange={handleSortChange}
+        >
+            <option value="">Default</option>
+            <option value="priceAsc">Price : Low to Hight</option>
+            <option value="priceDesc">Price : High to Low</option>
+            <option value="popularity">Popularity</option>
+        </select>
+    </div>
+  )
+}
+
+export default SortOptions
