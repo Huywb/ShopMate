b3f429275ef72a15ec81216a1d4b18aef9375c13
huywb <pthhuy1306@gmail.com>
2025-11-26 15:58:20 +0700

    Jsonwebtoken middleware

---
+++ backend/middleware/JWT.js
@@ -0,0 +1,31 @@
+import  jwt  from "jsonwebtoken"
+
+
+export const createJWTToken = (id)=>{
+    const token = jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'1d'})
+    return token;
+}
+
+export const verifyJWTToken = (req,res,next)=>{
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized" });
+        }
+        cosnt token = authHeader.split(" ")[1];
+        if(!token){
+            return res.status(401).json({message:"Unauthorized"})
+        }
+        jwt.verify(token, process.env.JWT_SECRET,(err,decoded)=>{
+            if(err){
+                return res.status(401).json({message:"Unauthorized"})
+            };
+            req.userId = decoded.id;
+            next();
+        })
+    } catch (error) {
+
+        return res.status(500).json({message:"Server Error",error:error.message})
+    }
+}
